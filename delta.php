303b73610385a0d4456b37385a84cfaf05a36658
naziradidier <nazirap20.aps1b@gmai.com>
2021-01-25 15:19:57 +0100

    resoudre delta

--- delta.php
+++ delta.php
@@ -1,33 +1,38 @@
 <?php
+    // $a= 2;
+    // $b = 2; 
+    // $c= 1;
+    //  $x1 = (-$b - sqrt(4))/(2*$a);
+    //  $x2 = (-$b + sqrt(4))/(2*$a);
+    //  echo $x1 . " </br>". $x2;
     function calculDelta($a, $b, $c){
-        $delta = pow(2, $b) - 4*$a*$c;
+        $delta = pow($b, 2) - 4*$a*$c;
 
         return $delta;
     }
-    
-    // echo $t;
 
     function resoudreDelta($a, $b, $c){
         $t = calculDelta($a, $b, $c);
+        echo $t;
+        echo ("</br>");
         if($t > 0){
             $x1 = (-$b - sqrt($t))/(2*$a);
             $x2 = (-$b + sqrt($t))/(2*$a);
-            echo $x1 . " ". $x2;
-         }
-        // elseif($t < 0){
-            
-        //     echo "pas de solution";
-        // }
-        // else {
-        //     $x= -$b/2*$a;
-        //     echo $x;
-        // }
-        return $t;
+            echo($x1);
+            echo ("</br>");
+            echo($x2);
+            return [$x1,$x2];
+        }
+        elseif($t ==  0){
+            $x = -$b/2*$a;
+            echo " la reponse est: ";
+            echo($x);
+            return $x;
+        }
+        else {
+            echo " pas de solution";
+        }
     }
     
-    $l = resoudreDelta(2, 1, 1);
-    echo $l;
-     
-
-
+    $l = resoudreDelta(2, 2, 1);
 ?>
