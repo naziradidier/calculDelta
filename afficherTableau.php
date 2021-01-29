eff89eb5d30d3abcaa2fd7970af2ecbcd2ae0af7
naziradidier <nazirap20.aps1b@gmai.com>
2021-01-29 09:51:28 +0100

    affichage d'un tableau

---
+++ afficherTableau.php
@@ -0,0 +1,12 @@
+<?php
+        $table = array(2,1,3,4,8,5,6,9,7,10);
+
+        function    affichierTableau($tableau){
+            for($i=0 ; $i < count($tableau); $i++){
+                echo $tableau[$i];
+            }
+        }
+    
+        $k = affichierTableau($table);
+        echo $k;
+?>
