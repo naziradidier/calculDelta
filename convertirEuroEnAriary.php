f2e788422444c5ee32235626ae419840db013d52
Nazirah <74833519+naziradidier@users.noreply.github.com>
2021-02-13 04:55:58 +0100

    Delete convertirEuroEnAriary.php

--- convertirEuroEnAriary.php
+++
@@ -1,8 +0,0 @@
-<?php
-     function convertirEuroEnAriary($euro){
-         $ariary = $euro * 4545.45;
-         return $ariary;
-     }
-     $k = convertirEuroEnAriary(2);
-     echo $k. " " . "Ariary";
-?>
