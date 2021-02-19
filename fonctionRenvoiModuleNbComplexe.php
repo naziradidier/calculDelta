f54ea1b4dab45719e815e6cce623a103eee606d1
Nazirah <74833519+naziradidier@users.noreply.github.com>
2021-02-19 07:36:37 +0100

    Delete fonctionRenvoiModuleNbComplexe.php

--- fonctionRenvoiModuleNbComplexe.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function fonctionRenvoiModuleNbComplexe($a, $b){
-        $moduleDeZ = sqrt(pow($a, 2) + pow($b, 2));
-        
-        return $moduleDeZ;
-    }
-    $t = fonctionRenvoiModuleNbComplexe(4,2);
-    echo "le module de Z est ". $t;
-?>
